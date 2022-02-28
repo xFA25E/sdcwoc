e33e7082c207cce33fdb74fd541b9d2464cbfc7b
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2022-02-28 13:06:33 +0100

    Refactor

--- sdcwoc.el
+++ sdcwoc.el
@@ -50,6 +50,11 @@
   :type 'string
   :group 'sdcwoc)
 
+(defcustom sdcwoc-mode-hook nil
+  "Hook run after entering SDCWOC mode."
+  :type 'hook
+  :group 'sdcwoc)
+
 ;;;;; FACES
 
 (defface sdcwoc-dictionary
@@ -61,6 +66,11 @@
   '((t :inherit font-lock-string-face))
    "SDCV word face."
    :group 'sdcwoc)
+
+(defface sdcwoc-dots
+  '((t :inherit font-lock-type-face :underline t))
+  "Face used to visualize sdcwoc dots."
+  :group 'sdcwoc)
 
 ;;;; VARIABLES
 
@@ -84,7 +94,7 @@
 (defun sdcwoc--shift-string (string &optional n)
   "Shift STRING to the right by N spaces.
 N by default is 2."
-  (replace-regexp-in-string (rx (or bos bol)) (make-string (or n 2) ?\ ) string))
+  (replace-regexp-in-string (rx (or bos bol)) (make-string (or n 2) ?\s) string))
 
 (defun sdcwoc--set-state-at-point (state)
   "Set state of entry at point to STATE."
@@ -98,15 +108,19 @@ N by default is 2."
   "Set all states of all entries to STATE."
   (ewoc-map (lambda (data) (setf (cdr data) state)) sdcwoc--ewoc))
 
+(defun sdcwoc--footer-p (node)
+  "Check whether NODE is ewoc footer."
+  (eq (ewoc--footer sdcwoc--ewoc) node))
+
 (defun sdcwoc--make-node-visible (node)
-  "Make ewoc NODE visible if part if it is hidden after screen."
-  (let ((next-node-location (if-let ((next-node (ewoc-next sdcwoc--ewoc node)))
-                                (ewoc-location next-node)
-                              (point-max))))
+  "Make ewoc NODE visible as much as possible."
+  (let* ((ewoc sdcwoc--ewoc)
+         (next-node (or (ewoc-next ewoc node) (ewoc--footer ewoc)))
+         (next-node-location (ewoc-location next-node)))
     (unless (pos-visible-in-window-p next-node-location)
-          (let ((node-line-number (line-number-at-pos (ewoc-location node)))
-                (next-node-line-number (line-number-at-pos next-node-location)))
-            (recenter (- (min (- next-node-line-number node-line-number) (window-height))))))))
+      (let ((node-line-number (line-number-at-pos (ewoc-location node)))
+            (next-node-line-number (line-number-at-pos next-node-location)))
+        (recenter (- (min (- next-node-line-number node-line-number) (window-height))))))))
 
 (defun sdcwoc--search (query)
   "Search QUERY in SDCV dictionaries."
@@ -121,12 +135,16 @@ N by default is 2."
                ((map ("dict" dictionary) ("word" word)) entry))
     (insert "--> " (propertize dictionary 'face 'sdcwoc-dictionary) " (" (propertize word 'face 'sdcwoc-word) ")")
     (pcase state
-      (:hidden (insert "..."))
-      ((let definition (sdcwoc--shift-string (string-trim (map-elt entry "definition"))))
+      (:hidden (insert (propertize "..." 'face 'sdcwoc-dots)))
+      ((let def (sdcwoc--shift-string (string-trim (map-elt entry "definition"))))
        (insert "\n\n")
        (pcase state
-         (:full (insert definition))
-         (:partial (insert (string-join (seq-take (split-string definition "\n" t) 2) "\n") "...")))))
+         (:full (insert def))
+         (:partial (if-let* ((pos1 (string-match-p "\n" def))
+                             (pos2 (string-match-p "\n" def (1+ pos1)))
+                             (pos3 (string-match-p "\n" def (1+ pos2))))
+                       (insert (substring def 0 pos2) "\n  " (propertize "..." 'face 'sdcwoc-dots))
+                     (insert def))))))
     (insert "\n")))
 
 ;;;; BINDINGS
@@ -146,24 +164,32 @@ N by default is 2."
 
 (define-derived-mode sdcwoc-mode special-mode "SDCWOC"
   "Mode used to draw SDCV entries."
-  :group 'sdcwoc)
+  :group 'sdcwoc
+  (buffer-disable-undo)
+  (with-silent-modifications (erase-buffer))
+  (setq sdcwoc--ewoc (ewoc-create #'sdcwoc--draw)))
 
 ;;;###autoload
 (defun sdcwoc (query)
   "Search QUERY in sdcv dictionaries."
   (interactive (list (sdcwoc--read-query)))
-  (let ((buffer (get-buffer sdcwoc-buffer-name)))
-    (unless buffer
-      (with-current-buffer (setq buffer (get-buffer-create sdcwoc-buffer-name))
-        (with-silent-modifications
-          (sdcwoc-mode)
-          (setq sdcwoc--ewoc (ewoc-create #'sdcwoc--draw)))))
-    (with-current-buffer buffer
-      (with-silent-modifications
-        (ewoc-filter sdcwoc--ewoc #'ignore)
-        (seq-do (lambda (entry) (ewoc-enter-last sdcwoc--ewoc (cons entry :partial)))
-                (sdcwoc--search query))))
-    (pop-to-buffer buffer '((display-buffer-reuse-window display-buffer-same-window)))))
+  (with-current-buffer (get-buffer-create sdcwoc-buffer-name)
+    (sdcwoc-mode)
+    (seq-doseq (entry (sdcwoc--search query))
+      (ewoc-enter-last sdcwoc--ewoc (cons entry :partial)))
+    (pop-to-buffer (current-buffer))))
+
+(defun sdcwoc-next-entry (n)
+  "Go to next Nth SDCV entry."
+  (interactive "p")
+  (let ((node (ewoc-goto-next sdcwoc--ewoc n)))
+    (unless (sdcwoc--footer-p node)
+      (sdcwoc--make-node-visible node))))
+
+(defun sdcwoc-previous-entry (n)
+  "Go to previous Nth SDCV entry."
+  (interactive "p")
+  (ewoc-goto-prev sdcwoc--ewoc n))
 
 (defun sdcwoc-expand ()
   "Expand SDCV entry at point."
@@ -194,16 +220,6 @@ N by default is 2."
   "Hide all SDCV entries."
   (interactive)
   (sdcwoc--set-all-states :hidden))
-
-(defun sdcwoc-next-entry (n)
-  "Go to next Nth SDCV entry."
-  (interactive "p")
-  (sdcwoc--make-node-visible (ewoc-goto-next sdcwoc--ewoc n)))
-
-(defun sdcwoc-previous-entry (n)
-  "Go to previous Nth SDCV entry."
-  (interactive "p")
-  (ewoc-goto-prev sdcwoc--ewoc n))
 
 ;;;; PROVIDE
 
